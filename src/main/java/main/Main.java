8692b409c99e8489df8a529b4d32b8e0775e6870
eugenyb <eberkunsky@gmail.com>
2023-05-23 17:26:46 +0300

    test1

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
 
     }
+
+    public double f(double x) {
+        return x + 2;
+    }
 }
---
+++ src/test/java/main/MainTest.java
@@ -0,0 +1,18 @@
+package main;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class MainTest {
+
+    @BeforeMethod
+    public void setUp() {
+    }
+
+    @Test
+    public void testF() {
+
+    }
+}
